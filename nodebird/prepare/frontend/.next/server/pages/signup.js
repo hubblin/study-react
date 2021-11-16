e92a15190141c38e66e8c0772577e4d435032330
hubblin <chdnjsdkvkxm18@naver.com>
2021-11-16 22:01:13 +0900

    node bird 강좌

---
+++ nodebird/prepare/frontend/.next/server/pages/signup.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/signup";
+exports.ids = ["pages/signup"];
+exports.modules = {
+
+/***/ "./pages/signup.js":
+/*!*************************!*\
+  !*** ./pages/signup.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ \"react/jsx-runtime\");\n/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nconst Signup = ()=>{\n    return(/*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(\"div\", {\n        __source: {\n            fileName: \"C:\\\\스터디\\\\studyReact\\\\nodebird\\\\prepare\\\\frontend\\\\pages\\\\signup.js\",\n            lineNumber: 2,\n            columnNumber: 12\n        },\n        __self: undefined,\n        children: \"회원가입 페이지\"\n    }));\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Signup);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9zaWdudXAuanMuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7OztBQUFBLEtBQUssQ0FBQ0EsTUFBTSxPQUFTLENBQUM7SUFDbEIsTUFBTSxzRUFBRUMsQ0FBRzs7Ozs7OztrQkFBQyxDQUFROztBQUN4QixDQUFDO0FBRUQsaUVBQWVELE1BQU0sRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL3JlYWN0LW5vZGViaXJkLWZyb250Ly4vcGFnZXMvc2lnbnVwLmpzP2NhOGMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgU2lnbnVwID0gKCkgPT4ge1xyXG4gICAgcmV0dXJuIDxkaXY+7ZqM7JuQ6rCA7J6FIO2OmOydtOyngDwvZGl2PlxyXG59XHJcblxyXG5leHBvcnQgZGVmYXVsdCBTaWdudXA7Il0sIm5hbWVzIjpbIlNpZ251cCIsImRpdiJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./pages/signup.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-runtime":
+/*!************************************!*\
+  !*** external "react/jsx-runtime" ***!
+  \************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/signup.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ nodebird/prepare/frontend/pages/index.js
@@ -0,0 +1,14 @@
+//pages는 무조건 pages여야한다.
+//코드 스플리팅 되기 때문이다.
+
+import AppLayout from "../components/AppLayout";
+
+const Home = () => {
+    return(
+        <AppLayout>
+            <div>Hello! Next</div>
+        </AppLayout>
+    )
+}
+
+export default Home;
