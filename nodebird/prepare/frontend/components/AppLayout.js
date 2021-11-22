b7d757cb7c27e5a0bae18a4fef3aedc2a3cfd5ec
hubblin <chdnjsdkvkxm18@naver.com>
2021-11-22 22:58:01 +0900

    styled 컴포넌트 적용

--- nodebird/prepare/frontend/components/AppLayout.js
+++ nodebird/prepare/frontend/components/AppLayout.js
@@ -2,13 +2,19 @@ import PropTypes from 'prop-types';
 import Link from 'next/link';
 import {Input, Menu, Row, Col} from 'antd';
 import React, {useState} from 'react';
+import styled from 'styled-components';
 //반응형을 디자인 할때는 모바일 먼저 하고 점점 키워야한다.
+
+const SearchInput = styled(Input.Search)`
+    vertical-align: middle
+`;
 
 import UserProfile from '../components/UserProfile';
 import LoginForm from '../components/LoginForm';
 
 const AppLayout = ({children}) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    
     return (
     <div>
         <Menu mode="horizontal">
@@ -19,7 +25,7 @@ const AppLayout = ({children}) => {
                 <Link href="/profile"><a>프로필</a></Link>
             </Menu.Item>
             <Menu.Item>
-                <Input.Search enterButton style={{verticalAlign: 'middle'}}/>
+                <SearchInput enterButton/>
             </Menu.Item>
             <Menu.Item>
                 <Link href="/signup"><a>회원가입</a></Link>
--- nodebird/prepare/frontend/components/LoginForm.js
+++ nodebird/prepare/frontend/components/LoginForm.js
@@ -2,6 +2,12 @@ import React from 'react';
 import {Button, Form, Input} from 'antd';
 import {useState, useCallback} from 'react';
 import Link from 'next/link';
+import styled from 'styled-components';
+
+const ButtonWrapper = styled.div`
+    margin-top : 10px;
+`
+
 const LoginForm = () => {
     const [id, setId] = useState('');
     const [password, setPassword] = useState('');
@@ -30,10 +36,10 @@ const LoginForm = () => {
                     required
                 />
             </div>
-            <div>
+            <ButtonWrapper>
                 <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
                 <Link href="/signup"><a><Button>회원가입</Button></a></Link>
-            </div>
+            </ButtonWrapper>
         </Form>
     );
 }
